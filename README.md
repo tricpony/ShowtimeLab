de89fae0ad02d56d4b5ba1f6c71f5eca8612bb38
Brian Arthur <tricpony@mac.com>
2022-08-10 23:20:05 -0400

    add unit tests

---
+++ README.md
@@ -0,0 +1,15 @@
+# README #
+
+Read below some comments about what you will find in ShowtimeLab.
+
+### Language ###
+The project uses a combination of UIKit, Combine, and SwiftUI.
+
+### Unit Tests ###
+Some Unit tests were included
+
+### Comments ###
+Attempts to use an ordinary SwiftUI Image failed with an invalid JSON error so I used a web view instead.
+Overall, there's plenty here that I would refine with more time.  
+My Xcode version is old because of my older macbook pro & Catalina V 10.15.7.
+I believe newer Xcode will prompt you to click a button to convert the project.
--- ShowtimeLabTests/ShowtimeLabTests.swift
+++ ShowtimeLabTests/ShowtimeLabTests.swift
@@ -9,25 +9,29 @@ import XCTest
 @testable import ShowtimeLab
 
 class ShowtimeLabTests: XCTestCase {
+    var comic: Comic?
+    let comicJSON = """
+    {\"safe_title\":\"safe_title\", \
+    \"alt\":\"alt\", \
+    \"img\":\"img\", \
+    }
+    """
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        let decoder = JSONDecoder()
+        guard let payload = comicJSON.data(using: .utf8) else { XCTFail(); return }
+        comic = try decoder.decode(Comic.self, from: payload)
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func testTitle() {
+        XCTAssertEqual(comic?.title, "safe_title")
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testAlt() {
+        XCTAssertEqual(comic?.alt, "alt")
     }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testImg() {
+        XCTAssertEqual(comic?.imageUrl, "img")
     }
-
 }
